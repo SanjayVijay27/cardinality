82c72776cbba32c21f3855aaa466509575f6dd93
Sanjay Vijay <27sanjayvijay@gmail.com>
2025-02-22 19:44:45 -0500

    working text parsing

--- table_functions.py
+++ table_functions.py
@@ -3,6 +3,8 @@ import pandas as pd
 def create_columns(df, text, id):
     pairs = text.split('\n')
     for pair in pairs:
+        if "::" not in pair:
+            continue
         key, value = pair.split('::')
         df.loc[df['id'] == id, key] = value
     return df
